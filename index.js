b4fba9fcdb29f9225d7625db6134ef75d5350a09
rot1024 <aayhrot@gmail.com>
2019-07-04 19:56:23 +0900

    fix

--- index.js
+++ index.js
@@ -5,37 +5,32 @@ export const useTransition = (isActive, timeout, opts) => {
     isActive ? "entered" : opts && opts.mountOnEnter ? "unmounted" : "exited"
   );
   const timeoutRef = useRef();
-  const startedAt = useRef();
-  const startedAt2 = useRef();
+  const willFinishAt = useRef();
 
   const timeout1 = useCallback(() => {
     setState(
       isActive ? "entered" : opts && opts.unmountOnExit ? "unmounted" : "exited"
     );
-    startedAt.current = undefined;
-    startedAt2.current = undefined;
+    willFinishAt.current = undefined;
   }, [isActive]);
 
   const timeout2 = useCallback(() => {
     setState("entered");
-    startedAt.current = undefined;
-    startedAt2.current = undefined;
+    willFinishAt.current = undefined;
   }, []);
 
   const timeout3 = useCallback(() => {
     setState(
       isActive ? "entered" : opts && opts.unmountOnExit ? "unmounted" : "exited"
     );
-    startedAt.current = undefined;
-    startedAt2.current = undefined;
+    willFinishAt.current = undefined;
   }, [isActive, !!opts && opts.unmountOnExit]);
 
   const timeout4 = useCallback(() => {
     setState(isActive ? "entering" : "exiting");
     timeoutRef.current = setTimeout(timeout3, timeout);
-    startedAt2.current = startedAt.current;
-    startedAt.current = Date.now();
-  }, [isActive, timeout, timeout3, startedAt.current]);
+    willFinishAt.current = Date.now() + timeout;
+  }, [isActive, timeout, timeout3]);
 
   useEffect(() => {
     if (
@@ -43,18 +38,17 @@ export const useTransition = (isActive, timeout, opts) => {
         ? state === "entered"
         : state === "exited" || state === "unmounted"
     ) {
-      startedAt.current = undefined;
-      startedAt2.current = undefined;
+      willFinishAt.current = undefined;
       return undefined;
     }
 
     const now = Date.now();
     const duration =
-      typeof startedAt.current === "undefined"
+      typeof willFinishAt.current === "undefined"
         ? timeout
-        : typeof startedAt2.current === "undefined"
-        ? Math.max(0, now - startedAt.current)
-        : Math.max(0, timeout - startedAt.current + now);
+        : isActive
+        ? willFinishAt - now
+        : timeout - (willFinishAt - now);
 
     if (!isActive && (state === "entered" || state === "entering")) {
       setState("exiting");
@@ -67,8 +61,7 @@ export const useTransition = (isActive, timeout, opts) => {
       timeoutRef.current = setTimeout(timeout4, 30);
     }
 
-    startedAt2.current = startedAt.current;
-    startedAt.current = now;
+    willFinishAt.current = now + duration;
 
     return () => {
       clearTimeout(timeoutRef.current);
